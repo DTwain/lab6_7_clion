e3ca6e9d6a68b619b5493f310d812215acd6ac09
david <obrejadavid8@gmail.com>
2024-04-18 15:01:19 +0300

    lab 7 trebuie facutt

---
+++ cmake-build-debug/CMakeFiles/lab6_7_clion.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/domain/carte.cpp" "CMakeFiles/lab6_7_clion.dir/domain/carte.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/domain/carte.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/domain/validator.cpp" "CMakeFiles/lab6_7_clion.dir/domain/validator.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/domain/validator.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/main.cpp" "CMakeFiles/lab6_7_clion.dir/main.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/main.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/repository/repo.cpp" "CMakeFiles/lab6_7_clion.dir/repository/repo.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/repository/repo.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/service/service_biblioteca.cpp" "CMakeFiles/lab6_7_clion.dir/service/service_biblioteca.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/service/service_biblioteca.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/teste/test_domain.cpp" "CMakeFiles/lab6_7_clion.dir/teste/test_domain.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/teste/test_domain.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/teste/test_repo.cpp" "CMakeFiles/lab6_7_clion.dir/teste/test_repo.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/teste/test_repo.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/teste/test_service.cpp" "CMakeFiles/lab6_7_clion.dir/teste/test_service.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/teste/test_service.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/teste/test_validator.cpp" "CMakeFiles/lab6_7_clion.dir/teste/test_validator.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/teste/test_validator.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/ui/ui_biblioteca.cpp" "CMakeFiles/lab6_7_clion.dir/ui/ui_biblioteca.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/ui/ui_biblioteca.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/utils/iterator.cpp" "CMakeFiles/lab6_7_clion.dir/utils/iterator.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/utils/iterator.cpp.obj.d"
+  "D:/INFO 2/SEM 2/OOP/lab6-7---oop/utils/myvector.cpp" "CMakeFiles/lab6_7_clion.dir/utils/myvector.cpp.obj" "gcc" "CMakeFiles/lab6_7_clion.dir/utils/myvector.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab6_7_clion.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab6_7_clion.dir/domain/carte.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/domain/carte.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/domain/validator.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/domain/validator.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/main.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/main.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/repository/repo.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/repository/repo.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/service/service_biblioteca.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/service/service_biblioteca.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/teste/test_domain.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/teste/test_domain.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/teste/test_repo.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/teste/test_repo.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/teste/test_service.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/teste/test_service.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/teste/test_validator.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/teste/test_validator.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/ui/ui_biblioteca.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/ui/ui_biblioteca.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/utils/iterator.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/utils/iterator.cpp.obj.d"
+  "CMakeFiles/lab6_7_clion.dir/utils/myvector.cpp.obj"
+  "CMakeFiles/lab6_7_clion.dir/utils/myvector.cpp.obj.d"
+  "lab6_7_clion.exe"
+  "lab6_7_clion.exe.manifest"
+  "lab6_7_clion.pdb"
+  "liblab6_7_clion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab6_7_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
